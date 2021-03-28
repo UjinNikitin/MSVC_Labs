0fdf59d28ef52c06157a551b3e49ec703548314d
Yevhenii Nikitin <erhytn9@ukr.net>
2021-03-28 22:00:16 +0300

    Add HZ

---
+++ LoggingSvc/Clients/Implementation/HazelcastStorageProviderClient.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hazelcast;
+using Hazelcast.DistributedObjects;
+using LoggingSvc.Models;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingSvc.Clients.Implementation
+{
+    public class HazelcastStorageProviderClient : IStorageProviderClient
+    {
+        public HazelcastStorageProviderClient(ILogger<HazelcastStorageProviderClient> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IEnumerable<string>> Get()
+        {
+            await SetClient();
+            await GetMap();
+
+            try
+            {
+                return await _map.GetValuesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+            finally
+            {
+                await _map.DisposeAsync();
+                await _client.DisposeAsync();
+            }
+        }
+
+        public async Task Insert(Message msg)
+        {
+            await SetClient();
+            await GetMap();
+
+            try
+            {
+                await _map.SetAsync(msg.Id, msg.Value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            finally
+            {
+                await _map.DisposeAsync();
+                await _client.DisposeAsync();
+            }
+        }
+
+
+        private async Task SetClient()
+        {
+            _client = await HazelcastClientFactory.StartNewClientAsync(new HazelcastOptions
+            {
+                Networking =
+                {
+                    Addresses =
+                    {
+                        "192.168.0.50:6001",
+                        "192.168.0.50:6002",
+                        "192.168.0.50:6003"
+                    }
+                }
+            });
+        }
+
+        private async Task GetMap()
+        {
+            _map = await _client.GetMapAsync<Guid, string>("Logging-map");
+        }
+
+
+        private IHazelcastClient _client;
+        private IHMap<Guid, string> _map;
+        private readonly ILogger<HazelcastStorageProviderClient> _logger;
+    }
+}
--- LoggingSvc/Startup.cs
+++ LoggingSvc/Startup.cs
@@ -21,7 +21,7 @@ namespace LoggingSvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IStorageProviderClient, LocalStorageProviderClient>();
+            services.AddSingleton<IStorageProviderClient, HazelcastStorageProviderClient>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
