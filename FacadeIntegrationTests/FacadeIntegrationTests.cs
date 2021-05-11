f60346e338e42ab0ded74269bff63eb7af84d1be
Yevhenii Nikitin <erhytn9@ukr.net>
2021-05-11 19:21:08 +0300

    add .drone.yml

---
+++ FacadeIntegrationTests/FacadeIntegrationTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FacadeIntegrationTests
+{
+    public class FacadeIntegrationTests
+    {
+        [Fact]
+        public void FacadeIntegrationTest()
+        {
+
+        }
+    }
+}
---
+++ FacadeUnitTests/UnitTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace FacadeUnitTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void UnitTest()
+        {
+
+        }
+    }
+}
